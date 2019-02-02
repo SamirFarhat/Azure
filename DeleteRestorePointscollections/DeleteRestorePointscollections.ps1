a79900f3c100c2722fcbd7457f38fe7c6e455078
samirfarhatx <s.farhat@veebaze.com>
2019-02-02 12:27:43 +0100

    first version

---
+++ DeleteRestorePointscollections/DeleteRestorePointscollections.ps1
@@ -0,0 +1,28 @@
+workflow Delete-RestorePointsCollections{
+    param([string[]]$Rgname)
+    $resources = Get-AzureRmResource -ResourceGroupName "$Rgname"
+    
+     
+    foreach -Parallel ($resource in $resources) {
+    $retry = $true
+    while ($retry){
+    Try
+    {
+    Write-Output "Removing $($resource.Name)"
+    $removeOps= Remove-AzureRmResource -ResourceId $resource.ResourceId -Force 
+    if ($removeOps) {$retry = $false}
+    }
+     
+    Catch
+    {
+    $retry = $true
+    }
+     
+    Finally {if ($retry) {write-output "Failed to delete $($resource.Name) --> Retrying"} else {write-output "Deletion of  $($resource.Name) successfull"}}
+     
+    }
+    }
+    }
+    
+    #Run the workflow
+    Delete-RestorePointsCollections -Rgname "Resource group Name"
